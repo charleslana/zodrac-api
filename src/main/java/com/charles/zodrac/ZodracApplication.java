73ad65cd55e25fad994ab81ea4d56f11f15ca2ab
Charles <charleslanop@gmail.com>
2022-09-22 22:40:57 -0300

    update openAPI

--- src/main/java/com/charles/zodrac/ZodracApplication.java
+++ src/main/java/com/charles/zodrac/ZodracApplication.java
@@ -1,5 +1,9 @@
 package com.charles.zodrac;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +11,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ZodracApplication {
+
+    @Bean
+    public OpenAPI customOpenApi(@Value("${app.version}") String appVersion,
+                                 @Value("${app.description}") String appDescription) {
+        return new OpenAPI().components(new Components())
+                .info(new io.swagger.v3.oas.models.info.Info().title("Zodrac API")
+                        .version(appVersion)
+                        .description(appDescription)
+                        .license(new License().name("Zodrac by Charles Lana")
+                                .url("https://github.com/charleslana")));
+    }
 
     @Bean
     public BCryptPasswordEncoder getEncoder() {
